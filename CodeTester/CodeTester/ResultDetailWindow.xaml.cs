9500138e6d904bdbced6477ab8fc561e6871d491
Jelly Kuo <jellykuo1234@gmail.com>
2018-11-22 12:49:53 +0800

    Add diff

--- CodeTester/CodeTester/ResultDetailWindow.xaml.cs
+++ CodeTester/CodeTester/ResultDetailWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using DiffMatchPatch;
 
 namespace CodeTester
 {
@@ -19,10 +20,43 @@ namespace CodeTester
     /// </summary>
     public partial class ResultDetailWindow : Window
     {
+        Result result;
+
         public ResultDetailWindow(Result result)
         {
             InitializeComponent();
+            this.result = result;
+            rtbAnswer.AppendText(result.Test.Answer.Content);
+            rtbOutput.AppendText(result.Output);
             DataContext = result;
+        }
+
+        private void BtnCopy_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(result.Test.Input.Content.Replace("\r", Environment.NewLine));
+            MessageBox.Show("Content copied!", result.Test.Name, MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void BtnDiff_Click(object sender, RoutedEventArgs e)
+        {
+            diff_match_patch DIFF = new diff_match_patch();
+
+            List<Diff> diffs = DIFF.diff_main(result.Test.Answer.Content, result.Output);
+            DIFF.diff_cleanupSemanticLossless(diffs);
+
+            rtbOutput.Document = new FlowDocument();
+            foreach (var d in diffs)
+            {
+                var range = new TextRange(rtbOutput.Document.ContentEnd, rtbOutput.Document.ContentEnd);
+                if (d.operation == Operation.DELETE)
+                    continue;
+                range.Text = d.text.Replace("\n", Environment.NewLine);
+                if (d.operation == Operation.INSERT)
+                    range.ApplyPropertyValue(TextElement.BackgroundProperty, Brushes.LightSalmon);
+                else
+                    range.ApplyPropertyValue(TextElement.BackgroundProperty, Brushes.White);
+
+            }
         }
     }
 }
